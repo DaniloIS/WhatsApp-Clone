189e4f3a26f318a9680df169f8fa5115c9436147
DaniloIS <dailosantos-pg@hotmail.com>
2022-02-16 17:03:18 -0300

    Listando contatos e Click em contato

--- src/model/User.js
+++ src/model/User.js
@@ -53,13 +53,21 @@ export class User extends Model {
 
     save() {
 
-        return this.findByEmail(this.email).set(this.toJSON());
+        return User.findByEmail(this.email).set(this.toJSON());
 
     }
 
     static getRef() {
 
         return Firebase.db().collection('/users');
+
+    }
+
+    static getContactsRef(id) {
+
+        return User.getRef()
+            .doc(id)
+            .collection('contacts');
 
     }
 
@@ -71,11 +79,37 @@ export class User extends Model {
 
     addContact(contact) {
 
-        return User.getRef()
-            .doc(this.email)
-            .collection('contacts')
+        return User.getContactsRef(this.email)
             .doc(btoa(contact.email))
             .set(contact.toJSON());
+
+    }
+
+    getContacts() {
+
+        return new Promise((s, f) => {
+
+            User.getContactsRef(this.email).onSnapshot(docs => {
+
+                let contacts = [];
+
+                docs.forEach(doc => {
+
+                    let data = doc.data();
+
+                    data.id = doc.id;
+
+                    contacts.push(data);
+
+                });
+
+                this.trigger('contactschange', contacts);
+
+                s(contacts);
+
+            });
+
+        });
 
     }
     
